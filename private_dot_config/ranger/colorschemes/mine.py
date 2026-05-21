4f82058286bbc14790fc90b527ac944f81670ad6
Michael Lamertz <michael.lamertz@gmail.com>
2026-05-21 23:55:35 +0200

    Ranger customizations

---
+++ private_dot_config/ranger/colorschemes/mine.py
@@ -0,0 +1,21 @@
+# This file is part of ranger, the console file manager.
+# License: GNU GPL version 3, see the file "AUTHORS" for details.
+
+from __future__ import (absolute_import, division, print_function)
+
+from ranger.colorschemes.default import Default
+from ranger.gui.color import yellow, white, reverse
+
+
+class Scheme(Default):
+    def use(self, context):
+        fg, bg, attr = Default.use(self, context)
+
+        if context.directory and not context.marked and not context.link \
+                and not context.inactive_pane:
+            fg = yellow
+
+        if context.marked:
+            fg = white
+
+        return fg, bg, attr
