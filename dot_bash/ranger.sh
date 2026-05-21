3f4ed420f0f963dca061dd8a9d497d58e6f742bb
Michael Lamertz <michael.lamertz@gmail.com>
2026-05-21 23:52:15 +0200

    Use packaged ranger instead of venv

--- dot_bash/ranger.sh
+++ dot_bash/ranger.sh
@@ -2,7 +2,7 @@ ranger () {
     fname=$( mktemp )
     trap "rm $fname" EXIT
 
-    command ~/.local/venvs/ranger/.venv/bin/ranger --choosedir=$fname "$@"
+    command ranger --choosedir=$fname "$@"
     dst=$( cat $fname )
     [[ -n "$dst" && -d "$dst" ]] && cd "$dst"
 }
